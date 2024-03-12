3bf3d6edd2666afd91dc5429656b3fb248e84f7c
nstefania <nstefaniagh@gmail.com>
2024-03-11 23:09:52 -0300

    practica tooltip1

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]'). tooltip();
+    $ ('#enviarCorreo'). click (function() {
+        alert('el correo fue enviado correctamente...');
+    });
+});
